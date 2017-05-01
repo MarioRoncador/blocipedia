2a4c0649a6070e221ca02a1a288e57dd1bf00413
MarioRoncador <mroncador@alaska.edu>
2017-05-01 17:46:42 -0400

    Assignment 5 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :wikis
+  after_initialize :init
+
+  enum role: [:admin, :standard, :premium]
+
+  def init
+    self.role  ||= "standard"           #will set the default value only if it's nil
+  end
+
 end
